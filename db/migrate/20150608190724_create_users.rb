aa92a1d5d86643c5a78a89728e97218ea1bd7fca
cireficc <christophercirefice@gmail.com>
2015-06-08 20:33:14 +0000

    User table migration

--- db/migrate/20150608190724_create_users.rb
+++ db/migrate/20150608190724_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.text :username
+      t.text :password
+      t.text :salt
       t.timestamps
     end
   end
