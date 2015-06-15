1229064dff7b4eff026e92f01a0f7f3f217fd5bd
cireficc <christophercirefice@gmail.com>
2015-06-15 17:39:39 +0000

    Set up roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
     
+    # User roles in the MLL department and the LRC:
+    # :director - Director and assistant director of the LRC. Highest privileges
+    # :labasst - Lab assistants of the LRC. Elevated privileges
+    # :faculty - Faculty in the MLL department who teach courses
+    # :student - Students in the MLL department who enroll in courses
+    enum role [:director, :labasst, :faculty, :student]
+    
     has_secure_password
 end
