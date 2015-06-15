89d5b727105d0425d46470281cb7785078aced6c
cireficc <christophercirefice@gmail.com>
2015-06-15 18:06:40 +0000

    Migration for first/last name and role, update seeds accordingly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608190724) do
+ActiveRecord::Schema.define(version: 20150615175903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20150608190724) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-    User.create(username: 'cireficc', g_number: 'g00749804', password: 'chris')
-    User.create(username: 'timmerk', g_number: 'g00000000', password: 'kevin')
+    User.create(username: 'cireficc', g_number: 'g00749804', password: 'chris', first_name: 'Chris', last_name: 'Cirefice', role: 'labasst')
+    User.create(username: 'timmerk', g_number: 'g00000000', password: 'kevin', first_name: 'Kevin', last_name: 'Timmer', role: 'director')
