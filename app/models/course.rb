a67a5bcc0bcd9e1e181f53a502406ee4baa898a7
cireficc <christophercirefice@gmail.com>
2015-06-19 16:35:17 +0000

    Migration for Courses and relevant seeds

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
 class Course < ActiveRecord::Base
+    
+    # Enum to describe the semester in which a course takes place.
+    # Although there are technically 3 summer semester types
+    # (6-week 1, 6-week 2, 12-week), classify them all as summer
+    # because that's way easier, and it doesn't really make a difference.
+    enum semester: [:fall, :winter, :summer]
+    
+    # Enum to describe the MLL department to which the course belongs.
+    # Basically, each language has its own department.
+    enum department: [:arabic, :chinese, :french, :german, :italian, :japanese, :russian, :spanish]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
     User.create(username: 'director', g_number: 'g00000000', password: 'director', first_name: 'Director', last_name: 'MLL', role: 'director')
     User.create(username: 'labasst', g_number: 'g00000000', password: 'labasst', first_name: 'Labasst', last_name: 'MLL', role: 'labasst')
-    User.create(username: 'faculty', g_number: 'g00000000', password: 'faculty', first_name: 'Faculty', last_name: 'MLL', role: 'faculty')
+    faculty = User.create(username: 'faculty', g_number: 'g00000000', password: 'faculty', first_name: 'Faculty', last_name: 'MLL', role: 'faculty')
     User.create(username: 'student', g_number: 'g00000000', password: 'student', first_name: 'Student', last_name: 'MLL', role: 'student')
+    
+    spa = Course.create(year: 2015, semester: 0, department: 7, course: 101, section: 01, name: 'SPA 101 01 - Elementary Spanish I')
+    spa.id = 12754
+    spa.save!
+    fra = Course.create(year: 2015, semester: 0, department: 2, course: 101, section: 01, name: 'FRE 101 01 - Elementary French I')
+    fra.id = 12775
+    fra.save!
