c137fc49a9370cb355c537b6672cc18edc7fb502
cireficc <christophercirefice@gmail.com>
2015-06-29 17:02:01 +0000

    Check if user id in session is nil before querying the database uselessly

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -9,7 +9,7 @@ module AccountHelper
     
     # Returns the current logged-in user (if any)
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+        @current_user ||= User.find_by(id: session[:user_id]) unless session[:user_id].nil?
     end
     
     # Returns true if the user is logged in, false otherwise
