7e0f238669e0eb9986f20b014277f111a6836094
cireficc <christophercirefice@gmail.com>
2015-07-01 20:53:36 +0000

    Rework project proposal form. Attach data attributes containing default number of training/editing events to project type

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,4 +11,24 @@ class Project < ActiveRecord::Base
     # :pages - A document-based project using Pages
     # :other - A different type of project that should be detailed in the project's description
     enum category: [:camtasia, :garage_band, :pixton, :photo_booth, :blog, :pages, :other]
+    
+    CATEGORY_TRAINING_EVENTS = {
+        camtasia: 3,
+        garage_band: 2,
+        pixton: 2,
+        photo_booth: 1,
+        blog: 2,
+        pages: 1,
+        other: 2
+    }
+    
+    CATEGORY_EDITING_EVENTS = {
+        camtasia: 4,
+        garage_band: 3,
+        pixton: 3,
+        photo_booth: 2,
+        blog: 2,
+        pages: 2,
+        other: 3
+    }
 end
