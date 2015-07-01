c34db8c09a6edf7f884b382715ce7cf864506b98
cireficc <christophercirefice@gmail.com>
2015-07-01 22:00:23 +0000

    On project type change, set num training and editing to defaults for the type (WIP)

--- app/assets/javascripts/projects/new_project.js
+++ app/assets/javascripts/projects/new_project.js
@@ -11,4 +11,36 @@ $(document).on('change', '#category', function() {
 	    $("#other_warning").removeClass("hidden");
 	else
 	    $("#other_warning").addClass("hidden");
+	    
+	var num_training = $(this).find(':selected').data('default-training');
+	var num_editing = $(this).find(':selected').data('default-editing');
+	
+	var training_html = generateEventHtml(num_training, "Training");
+	var editing_html = generateEventHtml(num_editing, "Editing");
+	
+	$("#training_reservations").empty().append(training_html);
+	$("#editing_reservations").empty().append(editing_html);
 });
+
+function generateEventHtml(num, type) {
+	
+	var type_lower = type.toLowerCase();
+	
+	var html = "";
+	
+	for (var i = 1; i <= num; i ++) {
+		html += '<div class="form-group">\
+  					<div class="col-md-2">\
+    					<h4>' + type + ' ' + i + '</h4>\
+  					</div>\
+  					<div class="col-md-5">\
+    					<input class="form-control" name="' + type_lower + '_start_' + i + '" type="text" />\
+  					</div>\
+  					<div class="col-md-5">\
+    					<input class="form-control" name="' + type_lower + '_end_' + i + '" type="text" />\
+  					</div>\
+				</div>';
+	}
+	
+	return html;
+}
