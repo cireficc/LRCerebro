c508a57c61f79f7120efdc8318a7e4b110ec4b79
cireficc <christophercirefice@gmail.com>
2015-07-08 21:00:34 +0000

    Add enums to project reservation model

--- app/models/project_reservation.rb
+++ app/models/project_reservation.rb
@@ -1,4 +1,15 @@
 class ProjectReservation < ActiveRecord::Base
     
     belongs_to :project
+    
+    # Project reservation categories
+    # :training - This is for training the students (project overview, teaching software, etc.)
+    # :editing - This is for editing the projects
+    enum category: [:training, :editing]
+    
+    # Project reservation categories
+    # :project_introduction - Introduce the project to the students, create accounts, demonstration/how-to's, etc.
+    # :camera_training - Train the students how to operate the cameras
+    # :camtasia_traning - Train the students how to use Camtasia Studio
+    enum subtype: [:project_introduction, :camera_training, :camtasia_traning]
 end
