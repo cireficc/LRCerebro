db52808e4d782437667869f4f090e4a117f049e4
cireficc <christophercirefice@gmail.com>
2015-07-17 16:20:19 +0000

    Finish project form_for rework. Create and save projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,23 @@ class ProjectsController < ApplicationController
     def new
         @project = Project.new
     end
+    
+    def create
+        @project = Project.new(project_params)
+        
+        puts "Project: " + @project.to_s
+        
+        if @project.save
+            flash[:success] = "Your project, " + @project.name + ", has been successfully submitted!"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    
+    def project_params
+        params.require(:project).permit(:course_id, :category, :name, :description, :script_due, :due, :viewable_by)
+    end
 end
