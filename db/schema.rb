857ed74ae6bd3d38cfd342f3e43edc457d87f8f5
cireficc <christophercirefice@gmail.com>
2015-07-17 16:36:10 +0000

    Add approved/archived defaults to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708150853) do
+ActiveRecord::Schema.define(version: 20150717162837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(version: 20150708150853) do
     t.datetime "script_due"
     t.datetime "due"
     t.datetime "viewable_by"
-    t.boolean  "approved"
+    t.boolean  "approved",     default: false
     t.string   "publish_link"
-    t.boolean  "archived"
+    t.boolean  "archived",     default: false
     t.datetime "archived_at"
     t.datetime "created_at"
     t.datetime "updated_at"
