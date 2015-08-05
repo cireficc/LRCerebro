65c733c90558337d465dfc795351ca831d86bd63
cireficc <christophercirefice@gmail.com>
2015-08-05 20:16:12 +0000

    Add comments to the signup variable/tab click

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     
+    // Check the value of the signup hidden field. If true, switch to the signup tab
     var signup = $("#signup_true").val();
     if (signup)
         $("#signup_tab").click();
