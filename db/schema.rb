568da539e9df9831547579bff8211090e97c8fbc
cireficc <christophercirefice@gmail.com>
2015-08-05 18:44:41 +0000

    WIP - change login flow to use :registered instead of relying on empty password hash

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717162837) do
+ActiveRecord::Schema.define(version: 20150805184255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150717162837) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "role"
+    t.boolean  "registered"
   end
 
 end
