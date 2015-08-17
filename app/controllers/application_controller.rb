7449a39343c459d4ffedaf9dda803bf49d460570
cireficc <christophercirefice@gmail.com>
2015-08-17 17:22:58 +0000

    Add Pundit authorization for projects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  # Resource authorization gem
+  include Pundit
   include ApplicationHelper
   include AccountHelper
+  
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:danger] = "You are not authorized to perform that action."
+    redirect_to(root_path)
+  end
 end
