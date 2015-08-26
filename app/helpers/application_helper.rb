c25c07979bab36dc40b525c00f312b07932584ba
cireficc <christophercirefice@gmail.com>
2015-08-26 21:41:34 +0000

    Allow for action-based rendering of add/delete fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
     
-    def generate_hidden_add_fields_data(f, association, object_defaults)
+    def generate_hidden_add_fields_data(f, association, object_defaults, controller_action)
         
         new_object = f.object.send(association).klass.new(object_defaults)
         id = new_object.object_id
         
         fields = f.fields_for(association, new_object, child_index: id) do |builder|
-          render(association.to_s.singularize + "_fields", f: builder)
+          render(controller_action.to_s + "_" + association.to_s.singularize + "_fields", f: builder)
         end
         
         hidden_field_tag('add_fields_data', nil, data: {id: id, fields: fields.gsub("\n", "")})
