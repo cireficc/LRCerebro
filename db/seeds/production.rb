d4e8e8aad5250bf4fabcf0cebd68579ce8f62537
cireficc <christophercirefice@gmail.com>
2015-08-31 03:34:46 +0000

    Move console prints to a logfile

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -2,15 +2,19 @@ mll_data_directory = Rails.root.join('db', 'MLL_data')
 lrc_ppl = mll_data_directory.join('lrc_ppl.txt')
 lrc_crs = mll_data_directory.join('lrc_crs.txt')
 lrc_enr = mll_data_directory.join('lrc_enr.txt')
-
-puts "Seeding production (using MLL data from Blackboard)..."
-puts "The files: lrc_ppl.txt, lrc_crs.txt and lrc_enr.txt must be located in: #{mll_data_directory}..."
-puts "NOTE: the directory /db/MLL_data is not (and should not) be checked into Git."
+log_file_path = mll_data_directory.join('log.txt')
 
 require 'csv'
 
-puts "\n"
-puts "Importing students and faculty from lrc_ppl.txt..."
+log_file = File.open(log_file_path, "w")
+log_file.puts "Log file for attempted MLL data import on #{Time.current.to_s}"
+log_file.puts
+log_file.puts "Seeding production (using MLL data from Blackboard)..."
+log_file.puts "The files: lrc_ppl.txt, lrc_crs.txt and lrc_enr.txt must be located in: #{mll_data_directory}..."
+log_file.puts "NOTE: the directory /db/MLL_data is not (and should not) be checked into Git."
+log_file.puts
+log_file.puts "[IMPORTING] students and faculty from lrc_ppl.txt..."
+log_file.puts
 
 # After import, we use these two arrays to remove users that are no longer active in MLL courses
 @imported_users = Array.new # The list of users being imported from the CSV file
@@ -34,20 +38,22 @@ CSV.foreach(lrc_ppl, col_sep: '|', headers: false).each_with_index do |row, i|
 	@user = User.create(username: username, g_number: g_number, first_name: first_name, last_name: last_name, role: role) if @user.nil?
 	
 	@imported_users << @user
-	
-	print "." if (i % 25 == 0)
 end
 
-puts "\n"
+log_file.puts "DONE"
+log_file.puts
+
 # Get a list of users that were in the database before import, but were not in the CSV, and delete them
 @users_to_delete = @existing_users - @imported_users
+log_file.puts "Deleting users that have been removed from the MLL data\n along with all course enrollment data for the user" if @users_to_delete.any?
 @users_to_delete.each do |u|
+	log_file.puts "Deleting user: #{u.username}"
 	u.courses.delete_all # Remove all courses from the user before deleting to keep the join table clean
 	u.delete
 end
 
-puts "\n"
-puts "Importing courses from lrc_crs.txt..."
+log_file.puts "[IMPORTING] courses from lrc_crs.txt..."
+log_file.puts
 
 # After import, we use these two arrays to remove courses that are no longer available (however unlikely that is)
 @imported_courses = Array.new # The list of courses being imported from the CSV file
@@ -81,20 +87,22 @@ CSV.foreach(lrc_crs, col_sep: '|', headers: false).each_with_index do |row, i|
 	@course.save!
 	
 	@imported_courses << @course
-	
-	print "." if (i % 25 == 0)
 end
 
-puts "\n"
+log_file.puts "DONE"
+log_file.puts
+
 # Get a list of courses that were in the database before import, but were not in the CSV, and delete them
 @courses_to_delete = @existing_courses - @imported_courses
+log_file.puts "Deleting courses that have been removed from the MLL data\n along with all user enrollment data for the course" if @courses_to_delete.any?
 @courses_to_delete.each do |c|
+	log_file.puts "Deleting course: #{c.name}"
 	c.users.delete_all # Remove all users from the course before deleting to keep the join table clean
 	c.delete
 end
 
-puts "\n"
-puts "Importing enrollments from lrc_enr.txt..."
+log_file.puts "[IMPORTING] enrollments from lrc_enr.txt..."
+log_file.puts
 
 # After import is finished for a course, we use these two arrays to remove enrolls that no longer exist
 @imported_enrolls = Array.new
@@ -110,7 +118,9 @@ CSV.foreach(lrc_enr, col_sep: '|', headers: false).each_with_index do |row, i|
 	# then reset the lists of imported enrolls and existing enrolls for the new course
 	if @course && @course.id != course_id
 		@enrolls_to_delete = @existing_enrolls - @imported_enrolls
+		log_file.puts "Deleting enrollment data that has been removed from the MLL data" if @enrolls_to_delete.any?
 		@enrolls_to_delete.each do |u|
+			log_file.puts "Deleting enrolled: user [#{c.name}] from course [#{@course.name}]"
 			@course.users.delete(u)
 		end
 		
@@ -129,7 +139,8 @@ CSV.foreach(lrc_enr, col_sep: '|', headers: false).each_with_index do |row, i|
 		@course.users << @user unless @existing_enrolls.include?(@user)
 		@imported_enrolls << @user
 	end
-	
-	print "." if (i % 25 == 0)
 end
-puts "\n"
+
+log_file.puts "DONE"
+
+log_file.close
