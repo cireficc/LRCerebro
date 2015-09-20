755871ad07a2a4cb17937132f4723f3759d0eada
cireficc <christophercirefice@gmail.com>
2015-09-20 21:01:38 +0000

    Accept nested attributes for users in courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
     
     has_and_belongs_to_many :users
+    accepts_nested_attributes_for :users
     has_many :projects
     
     # Enum to describe the semester in which a course takes place.
