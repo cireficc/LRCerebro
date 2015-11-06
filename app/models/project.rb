1ac07b1e6bd9eb1890c4a29e61ce312e3944d6b8
cireficc <christophercirefice@gmail.com>
2015-11-06 21:16:50 +0000

    Cascade delete ProjectReservation children when a Project is deleted

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
     
     belongs_to :course
-    has_many :project_reservations
+    has_many :project_reservations, :dependent => :destroy
     accepts_nested_attributes_for :project_reservations, :reject_if => lambda { |a| a[:start].blank? && a[:end].blank? }, :allow_destroy => true
     validates :course_id, :category, :name, :description, :script_due, :due, presence: true
     attr_accessor :present
