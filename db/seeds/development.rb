360da44f9198476827df35c84c6ce36f549bd3c0
cireficc <christophercirefice@gmail.com>
2015-11-23 16:01:36 +0000

    Added a lot more seeds for users, courses, projects and project reservations

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -7,12 +7,16 @@ faculty = User.create(username: 'faculty', g_number: 'g00000000', password: 'fac
 student = User.create(username: 'student', g_number: 'g00000000', password: 'student', first_name: 'Student', last_name: 'MLL', role: User.roles[:student], registered: true)
 User.create(username: 'unregistered', g_number: 'g00000000', first_name: 'Unregistered', last_name: 'MLL', role: User.roles[:student], registered: false)
 wardse = User.create(username: 'wardse', g_number: 'g00000000', first_name: 'Séverine', last_name: 'Ward', role: User.roles[:faculty], registered: false)
+fuchsk = User.create(username: 'fuchsk', g_number: 'g00000000', first_name: 'Kevin', last_name: 'Fuchs', role: User.roles[:faculty], registered: true)
 cireficc = User.create(username: 'cireficc', g_number: 'g00749804', first_name: 'Chris', last_name: 'Cirefice', role: User.roles[:student], registered: false)
 
 # Create a few courses for users to be enrolled in
 spa = Course.create(year: 2015, semester: 0, department: 7, course: 101, section: 01, name: 'SPA 101 01 - Elementary Spanish I')
 spa.id = 12754
 spa.save!
+spa2 = Course.create(year: 2015, semester: 0, department: 7, course: 102, section: 03, name: 'SPA 102 03 - Elementary Spanish II')
+spa2.id = 12755
+spa2.save!
 fre = Course.create(year: 2015, semester: 0, department: 2, course: 101, section: 01, name: 'FRE 101 01 - Elementary French I')
 fre.id = 12775
 fre.save!
@@ -25,15 +29,24 @@ fre3.save!
 Course.create(year: 2015, semester: 0, department: 8, course: 100, section: 00, name: 'LRC 100 00 - Intro to LRC')
 
 # Create a few projects to assign to courses
-spa_project = Project.create(name: "SPA Project", description: "Camtasia description", category: Project.categories[:camtasia], num_groups: 5,
+spa_project_1 = Project.create(name: "SPA Project", description: "Camtasia description", category: Project.categories[:camtasia], num_groups: 5,
                                 script_due: DateTime.new(2015,8,15,7,0,0,'-5'), due: DateTime.new(2015,8,25,16,0,0,'-5'),
                                 viewable_by: DateTime.new(2015,8,27,13,0,0,'-5'), approved: false)
+spa_project_2 = Project.create(name: "Bienvenido", description: "Some type of Garage Band project", category: Project.categories[:garage_band], num_groups: 8,
+                                script_due: DateTime.new(2015,8,17,7,0,0,'-5'), due: DateTime.new(2015,8,29,18,0,0,'-5'),
+                                viewable_by: DateTime.new(2015,8,30,13,0,0,'-5'), approved: true)
+spa_project_3 = Project.create(name: "Muy Caliente", description: "Pixton project", category: Project.categories[:pixton], num_groups: 9,
+                                script_due: DateTime.new(2015,8,11,7,0,0,'-5'), due: DateTime.new(2015,8,24,16,0,0,'-5'),
+                                viewable_by: DateTime.new(2015,8,26,13,0,0,'-5'), approved: false)
 fre_project_1 = Project.create(name: "FRE Project 1", description: "Garage Band description", category: Project.categories[:garage_band], num_groups: 6,
                                 script_due: DateTime.new(2015,8,13,7,0,0,'-5'), due: DateTime.new(2015,8,23,16,0,0,'-5'),
                                 viewable_by: DateTime.new(2015,8,25,13,0,0,'-5'), approved: false)
 fre_project_2 = Project.create(name: "FRE Project 2", description: "Pixton description", category: Project.categories[:pixton], num_groups: 3,
                                 script_due: DateTime.new(2015,8,13,7,0,0,'-5'), due: DateTime.new(2015,8,23,16,0,0,'-5'),
                                 viewable_by: DateTime.new(2015,8,25,13,0,0,'-5'), approved: true)
+fre_project_3 = Project.create(name: "FRE Project 3", description: "Camtasia awesomeness", category: Project.categories[:camtasia], num_groups: 7,
+                                script_due: DateTime.new(2015,8,5,7,0,0,'-5'), due: DateTime.new(2015,8,29,16,0,0,'-5'),
+                                viewable_by: DateTime.new(2015,8,30,13,0,0,'-5'), approved: false)
                             
 # Create some reservations for a project
 training_1 = ProjectReservation.create(start: DateTime.new(2015,8,17,7,0,0,'-5'), end: DateTime.new(2015,8,17,7,50,0,'-5'),
@@ -44,21 +57,81 @@ training_2 = ProjectReservation.create(start: DateTime.new(2015,8,18,7,0,0,'-5')
                                         lab: Lab.locations[:b], category: ProjectReservation.categories[:training],
                                         subtype: ProjectReservation.subtypes[:camera_training],
                                         staff_notes: "Reserve a camera and tripod 1 hour before reservation.", faculty_notes: "Camera training necessary.")
+training_3 = ProjectReservation.create(start: DateTime.new(2015,8,19,6,0,0,'-5'), end: DateTime.new(2015,8,19,6,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training],
+                                        staff_notes: "Make sure Pixton accounts are set up", faculty_notes: "Intro to Pixton")
+training_4 = ProjectReservation.create(start: DateTime.new(2015,8,19,7,0,0,'-5'), end: DateTime.new(2015,8,19,8,15,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training],
+                                        faculty_notes: "Intro to Garage Band")
+training_5 = ProjectReservation.create(start: DateTime.new(2015,8,7,7,0,0,'-5'), end: DateTime.new(2015,8,7,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training])
+training_6 = ProjectReservation.create(start: DateTime.new(2015,8,9,7,0,0,'-5'), end: DateTime.new(2015,8,9,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training],
+                                        staff_notes: "No students have experience with Camtasia", faculty_notes: "Complete how-to is necessary.")
+training_7 = ProjectReservation.create(start: DateTime.new(2015,8,17,7,0,0,'-5'), end: DateTime.new(2015,8,17,7,50,0,'-5'),
+                                        lab: Lab.locations[:a], category: ProjectReservation.categories[:training],
+                                        subtype: ProjectReservation.subtypes[:project_introduction],
+                                        staff_notes: "Set up Google Drive mini-presentation.", faculty_notes: "Help me with Google Drive please!")
+training_8 = ProjectReservation.create(start: DateTime.new(2015,8,18,7,0,0,'-5'), end: DateTime.new(2015,8,18,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training],
+                                        subtype: ProjectReservation.subtypes[:camera_training],
+                                        staff_notes: "Reserve a camera and tripod 1 hour before reservation.", faculty_notes: "Camera training necessary.")
+training_9 = ProjectReservation.create(start: DateTime.new(2015,8,19,6,0,0,'-5'), end: DateTime.new(2015,8,19,6,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training],
+                                        staff_notes: "Make sure Pixton accounts are set up", faculty_notes: "Intro to Pixton")
+training_10 = ProjectReservation.create(start: DateTime.new(2015,8,19,7,0,0,'-5'), end: DateTime.new(2015,8,19,8,15,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training],
+                                        faculty_notes: "Intro to Garage Band")
+training_11 = ProjectReservation.create(start: DateTime.new(2015,8,7,7,0,0,'-5'), end: DateTime.new(2015,8,7,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:training])
 editing_1 = ProjectReservation.create(start: DateTime.new(2015,8,19,7,0,0,'-5'), end: DateTime.new(2015,8,19,7,50,0,'-5'),
                                         lab: Lab.locations[:a], category: ProjectReservation.categories[:editing])
+editing_2 = ProjectReservation.create(start: DateTime.new(2015,8,20,7,0,0,'-5'), end: DateTime.new(2015,8,20,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
+editing_3 = ProjectReservation.create(start: DateTime.new(2015,8,20,6,0,0,'-5'), end: DateTime.new(2015,8,20,6,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
+editing_4 = ProjectReservation.create(start: DateTime.new(2015,8,20,5,0,0,'-5'), end: DateTime.new(2015,8,20,5,50,0,'-5'),
+                                        lab: Lab.locations[:a], category: ProjectReservation.categories[:editing])
+editing_5 = ProjectReservation.create(start: DateTime.new(2015,8,21,7,0,0,'-5'), end: DateTime.new(2015,8,21,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
+editing_6 = ProjectReservation.create(start: DateTime.new(2015,8,18,7,0,0,'-5'), end: DateTime.new(2015,8,18,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
+editing_7 = ProjectReservation.create(start: DateTime.new(2015,8,19,7,0,0,'-5'), end: DateTime.new(2015,8,19,7,50,0,'-5'),
+                                        lab: Lab.locations[:a], category: ProjectReservation.categories[:editing])
+editing_8 = ProjectReservation.create(start: DateTime.new(2015,8,20,7,0,0,'-5'), end: DateTime.new(2015,8,20,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
+editing_9 = ProjectReservation.create(start: DateTime.new(2015,8,20,6,0,0,'-5'), end: DateTime.new(2015,8,20,6,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
+editing_10 = ProjectReservation.create(start: DateTime.new(2015,8,20,5,0,0,'-5'), end: DateTime.new(2015,8,20,5,50,0,'-5'),
+                                        lab: Lab.locations[:a], category: ProjectReservation.categories[:editing])
+editing_11 = ProjectReservation.create(start: DateTime.new(2015,8,21,7,0,0,'-5'), end: DateTime.new(2015,8,21,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
+editing_12 = ProjectReservation.create(start: DateTime.new(2015,8,18,7,0,0,'-5'), end: DateTime.new(2015,8,18,7,50,0,'-5'),
+                                        lab: Lab.locations[:b], category: ProjectReservation.categories[:editing])
                                                     
-# Assign the reservations to the project
-spa_project.project_reservations << training_1 << training_2 << editing_1
+# Assign the reservations to the projects
+spa_project_1.project_reservations << training_1 << training_2 << training_3 << editing_1 << editing_2
+spa_project_2.project_reservations << training_4 << editing_6 << editing_5
+spa_project_3.project_reservations << training_7 << training_9 << editing_7 << editing_8
+fre_project_1.project_reservations << training_8 << training_10 << editing_3 << editing_9
+fre_project_2.project_reservations << training_11 << editing_4 << editing_10
+fre_project_3.project_reservations << training_5 << training_6 << editing_11 << editing_12
                             
 # Assign the projects to the courses
-spa.projects << spa_project
-fre.projects << fre_project_1 << fre_project_2
+spa.projects << spa_project_1
+spa2.projects << spa_project_2 << spa_project_3
+fre.projects << fre_project_1
+fre2.projects << fre_project_2
+fre3.projects << fre_project_3
 
 # Assigning the course to the faculty means that the faculty is also enrolled in the course
 faculty.courses << spa << fre
 student.courses << spa << fre
 wardse.courses << fre2 << fre3
+fuchsk.courses << spa2
 cireficc.courses << fre3
 # Adding the faculty as a user of the course also triggers this relationship
 #spa.users << faculty << student
 #fre.users << faculty << student
+
+puts "Done"
