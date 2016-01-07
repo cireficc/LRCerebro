097cc831ba9e08fc0330d5578522a342136db0ba
cireficc <christophercirefice@gmail.com>
2016-01-07 00:02:50 +0000

    Finish converting has_and_belongs_to_many association to has_many :through, and let user_id in the join table be a string foreign key referencing users.g_number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105233243) do
+ActiveRecord::Schema.define(version: 20160106233810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160105233243) do
   end
 
   create_table "enrollments", id: false, force: true do |t|
-    t.integer "user_id",   null: false
+    t.string  "user_id",   null: false
     t.integer "course_id", null: false
   end
 
