da280010d10c43df8335de207004a6684537dcd0
cireficc <christophercirefice@gmail.com>
2016-01-08 00:38:53 +0000

    Add archived columns to Course and User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107235847) do
+ActiveRecord::Schema.define(version: 20160108003621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160107235847) do
     t.integer "course"
     t.integer "section"
     t.string  "name"
+    t.boolean "archived",   default: false
   end
 
   create_table "enrollments", force: true do |t|
@@ -74,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160107235847) do
     t.string   "last_name"
     t.integer  "role"
     t.boolean  "registered"
+    t.boolean  "archived",        default: false
   end
 
 end
