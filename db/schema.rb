828e151d9723e582ce095bdc0c62523c73543299
cireficc <christophercirefice@gmail.com>
2016-02-07 06:46:51 +0000

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108003621) do
+ActiveRecord::Schema.define(version: 20160205190410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20160108003621) do
     t.text     "faculty_notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "google_calendar_event_id"
+    t.string   "google_calendar_html_link"
   end
 
   create_table "projects", force: true do |t|
