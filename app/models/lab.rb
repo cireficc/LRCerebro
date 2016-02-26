e436bd83ebd3bc592dfc2a8feb1247d594a1520b
cireficc <christophercirefice@gmail.com>
2016-02-26 18:49:21 +0000

    Add Lab C/A to Lab locations enum

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -5,12 +5,14 @@ class Lab < ActiveRecord::Base
     # :a - Lab A (the lab with the main doors)
     # :b - Lab B (the lab in the middle)
     # :c - Lab C (the smallest lab in the back)
-    enum location: [:a, :b, :c]
+    # :c_a - Lab C/A (the students are split between Lab C and Lab A)
+    enum location: [:a, :b, :c, :c_a]
     
     # The total number of student workstations in each lab
     WORKSTATIONS = {
         a: 20,
         b: 30,
-        c: 16
+        c: 16,
+        c_a: 36
     }
 end
