cdee8f6e65f186ce66efc8485e11ecf1c4fca8fc
cireficc <christophercirefice@gmail.com>
2016-05-09 21:55:18 +0000

    Add attributes to Configuration model

--- db/migrate/20160509155500_create_configurations.rb
+++ db/migrate/20160509155500_create_configurations.rb
@@ -1,7 +1,13 @@
 class CreateConfigurations < ActiveRecord::Migration
   def change
     create_table :configurations do |t|
-
+      t.text :enrollment_update_message
+      t.datetime :enrollment_last_updated
+      t.datetime :current_semester_start
+      t.datetime :current_semester_end
+      t.text :class_project_submission_start
+      t.datetime :class_project_submission_end
+      t.text :class_project_deadline_message
       t.timestamps null: false
     end
   end
