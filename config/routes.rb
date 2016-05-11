2c155bad57ee4f16e56390bf8e3a66b82d3bbd06
cireficc <christophercirefice@gmail.com>
2016-05-11 18:21:35 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
   resources :standard_reservations
   
   # App live configuration
-  get 'configuration' => 'configuration#load'
-  post 'configuration' => 'configuration#save'
+  get 'configuration' => 'application_configurations#edit'
+  patch 'configuration' => 'application_configurations#update'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
