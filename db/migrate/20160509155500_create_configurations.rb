28d6c1106fbdcc786364fbe6902c8343d374d902
cireficc <christophercirefice@gmail.com>
2016-05-11 14:59:01 +0000

    Fix column types

--- db/migrate/20160509155500_create_configurations.rb
+++ db/migrate/20160509155500_create_configurations.rb
@@ -5,7 +5,7 @@ class CreateConfigurations < ActiveRecord::Migration
       t.datetime :enrollment_last_updated
       t.datetime :current_semester_start
       t.datetime :current_semester_end
-      t.text :class_project_submission_start
+      t.datetime :class_project_submission_start
       t.datetime :class_project_submission_end
       t.text :class_project_deadline_message
       t.timestamps null: false
