5ca49b39aa94191fafaf9b223b90442021b31a3e
Christopher Cirefice <christophercirefice@gmail.com>
2016-05-24 21:15:21 +0000

    Create Film model

---
+++ db/migrate/20160524211112_create_films.rb
@@ -0,0 +1,20 @@
+class CreateFilms < ActiveRecord::Migration
+  def change
+    create_table :films do |t|
+      t.integer :film_type
+      t.string :english_title
+      t.string :foreign_title
+      t.text :description
+      t.text :audio_languages, array: true, default: []
+      t.text :subtitle_languages, array: true, default: []
+      t.text :directors, array: true, default: []
+      t.text :cast_members, array: true, default: []
+      # :genres (acts_as_taggable)
+      t.string :released
+      t.integer :year
+      t.integer :length
+      t.string :mpaa_raing
+      t.timestamps null: false
+    end
+  end
+end
