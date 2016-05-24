75419adbd7b9845bd3c243bc4a822946d4e9f86c
Christopher Cirefice <christophercirefice@gmail.com>
2016-05-24 21:10:42 +0000

    Create InventoryItem polymorphic model

---
+++ db/migrate/20160520193401_create_inventory_items.rb
@@ -0,0 +1,14 @@
+class CreateInventoryItems < ActiveRecord::Migration
+  def change
+    create_table :inventory_items do |t|
+      t.string :catalog_number
+      t.string :catalog_code
+      t.integer :status
+      t.text :status_description
+      t.text :notes
+      t.belongs_to :inventoriable, polymorphic: true
+      t.timestamps null: false
+    end
+    add_index :inventory_items, [:inventoriable_id, :inventoriable_type], name: "inventory_items_polymorphic_association_index"
+  end
+end
