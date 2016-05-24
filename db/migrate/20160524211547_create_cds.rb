8e8991548a62c878d5647e64635f3e8cf862056d
Christopher Cirefice <christophercirefice@gmail.com>
2016-05-24 21:35:45 +0000

    Add language to CD model

--- db/migrate/20160524211547_create_cds.rb
+++ db/migrate/20160524211547_create_cds.rb
@@ -1,6 +1,7 @@
 class CreateCds < ActiveRecord::Migration
   def change
     create_table :cds do |t|
+      t.integer :language
       t.string :artist
       t.string :album
       t.integer :year
