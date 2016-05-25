a653346df28d500336c59f2475e8ab3c970f0852
Christopher Cirefice <christophercirefice@gmail.com>
2016-05-25 18:30:06 +0000

    Add scopes to InventoryItem for each polymorphic type

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,3 +1,9 @@
 class InventoryItem < ActiveRecord::Base
+    
     belongs_to :inventoriable, polymorphic: true
+    
+    scope :films, -> { where(inventoriable_type: "Film") }
+    scope :cds, -> { where(inventoriable_type: "CD") }
+    scope :games, -> { where(inventoriable_type: "Game") }
+    scope :equipment, -> { where(inventoriable_type: "Equipment") }
 end
