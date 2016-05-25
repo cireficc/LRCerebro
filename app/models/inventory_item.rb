6167602ce94e02c4a9a8f763282ac6fe5e6727d1
Christopher Cirefice <christophercirefice@gmail.com>
2016-05-25 22:13:06 +0000

    Add enums to InventoryItem

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -6,4 +6,14 @@ class InventoryItem < ActiveRecord::Base
     scope :cds, -> { where(inventoriable_type: "CD") }
     scope :games, -> { where(inventoriable_type: "Game") }
     scope :equipment, -> { where(inventoriable_type: "Equipment") }
+    
+    # Inventory status
+    # :functional - Nothing is wrong with the item
+    # :not_functional - The item is broken or not functional
+    # :missing - The item is missing from the LRC
+    enum status: {
+        functional: 0,
+        not_functional: 1,
+        missing: 2
+    }
 end
