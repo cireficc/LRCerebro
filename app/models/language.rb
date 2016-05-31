6350b1cf93258e23d463158b583bfd9d02fb0dc0
Christopher Cirefice <christophercirefice@gmail.com>
2016-05-31 16:26:44 +0000

    Move language validation to Language model and validate subtitle languages

--- app/models/language.rb
+++ app/models/language.rb
@@ -13,4 +13,26 @@ class Language < ActiveRecord::Base
         spanish: 8,
         other: 9
     }
+    
+    # Pass in a field (value) and the field name to validate the language enum across multiple models
+    # Return the error message to the caller to use with errors.add
+    def self.validate_languages(field)
+        
+        # This is not the greatest idea, but it works. Remove the empty string during validation
+        field.reject! { |l| l.empty? }
+        
+        if !field.any?
+            return "need to select a language"
+        end
+        
+        # If any of the audio languages is invalid, add the error and break
+        field.each do |f|
+            valid = false
+            Language.languages.each do |l, i|
+                valid = true if (f.downcase == l.downcase)
+            end
+            
+            return "not a valid language selection" unless valid
+        end
+    end
 end
