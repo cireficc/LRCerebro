d622b8e9439dfc7c14045c07eb4fa17801a3ea78
Christopher Cirefice <christophercirefice@gmail.com>
2016-06-06 22:03:52 +0000

    Use ILIKE for a case-insensitive search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
     def index
         @tags = ActsAsTaggableOn::Tag
-                .where("name LIKE ?", "#{params[:name]}%")
+                .where("name ILIKE ?", "#{params[:name]}%")
                 .where.not(name: params[:tags_chosen])
                 .includes(:taggings)
                 .where(taggings: {taggable_type: params[:taggable_type], context: params[:context] })
