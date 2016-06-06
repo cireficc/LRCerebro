2df4eeb9181ad6f2c8315939d5c5319b7121bdb9
Christopher Cirefice <christophercirefice@gmail.com>
2016-06-06 21:25:40 +0000

    Exclude tags that have already been chosen

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
     def index
-        @tags = ActsAsTaggableOn::Tag.where("name LIKE ?", "#{params[:name]}%")
+        @tags = ActsAsTaggableOn::Tag
+                .where("name LIKE ?", "#{params[:name]}%")
+                .where.not(name: params[:tags_chosen])
                 .includes(:taggings)
                 .where(taggings: {taggable_type: params[:taggable_type], context: params[:context] })
         render json: @tags
