01bdb5e59b0fb75528a67c902cb2945872bb95e6
Christopher Cirefice <christophercirefice@gmail.com>
2016-06-29 21:46:53 +0000

    Whitelist cover attributes

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -119,7 +119,7 @@ class FilmsController < ApplicationController
         def film_params
             params[:inventory_item][:film].permit(:film_type, :english_title, :foreign_title,
                     :transliterated_foreign_title, :description, :year, :length, :mpaa_rating,
-                    :director_list, :cast_member_list, :genre_list, audio_languages: [],
-                    subtitle_languages: [])
+                    :director_list, :cast_member_list, :genre_list, :cover, :cover_cache,
+                    :remote_cover_url, audio_languages: [], subtitle_languages: [])
         end
 end
