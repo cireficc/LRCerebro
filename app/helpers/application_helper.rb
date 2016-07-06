a364c7ffe12eb206d8e1df5c48cbc76e376de14a
Christopher Cirefice <christophercirefice@gmail.com>
2016-07-06 22:31:25 +0000

    More flexible nested fields generation helper
    Allow the nested fields form field generation helper to optionally use the controller action and current user's role in determining the path to the partial to render.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,19 @@ module ApplicationHelper
     
     LOCAL_TIME_ZONE = "America/Detroit"
     
-    def generate_hidden_add_fields_data(f, association, object_defaults, controller_action)
+    def generate_hidden_add_fields_data(f, association, object_defaults, controller_action, ignore_role=false)
         
         new_object = f.object.send(association).klass.new(object_defaults)
         id = new_object.object_id
         
         fields = f.simple_fields_for(association, new_object, child_index: id) do |builder|
-          render("#{@view_path}/#{controller_action.to_s}_#{association.to_s.singularize}_fields", f: builder)
+          role_path = (ignore_role) ? "" : "#{@view_path}/"
+          controller_action_path = (controller_action) ? "#{controller_action.to_s}_" : ""
+          path = "#{role_path}#{controller_action_path}#{association.to_s.singularize}_fields"
+          
+          render(path, f: builder)
         end
-        
+          
         hidden_field_tag('add_fields_data', nil, data: {id: id, fields: fields.gsub("\n", "")})
     end
   
