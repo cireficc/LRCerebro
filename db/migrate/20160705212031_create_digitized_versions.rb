41c95e5e92c92a9a748291506e05c9f4386560d1
Christopher Cirefice <christophercirefice@gmail.com>
2016-07-06 20:34:53 +0000

    Convert DigitizedVersion languages to string columns
    Rails 4 can't have two enums with any same values on the same model; e.g. audio/subtitle languages both want to use Language.languages; only Rails 5 has prefixes/suffixes for enums

--- db/migrate/20160705212031_create_digitized_versions.rb
+++ db/migrate/20160705212031_create_digitized_versions.rb
@@ -2,8 +2,8 @@ class CreateDigitizedVersions < ActiveRecord::Migration
   def change
     create_table :digitized_versions do |t|
       t.belongs_to :film
-      t.integer :audio_language
-      t.integer :subtitle_language
+      t.string :audio_language
+      t.string :subtitle_language
       t.string :direct_link
       t.text :embed_code
       t.timestamps null: false
