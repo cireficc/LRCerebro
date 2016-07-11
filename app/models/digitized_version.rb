936010e57b1249af88eac3609e31ad6da7633929
Christopher Cirefice <christophercirefice@gmail.com>
2016-07-11 20:11:50 +0000

    Add method to generate title for Ensemble

--- app/models/digitized_version.rb
+++ app/models/digitized_version.rb
@@ -3,4 +3,17 @@ class DigitizedVersion < ActiveRecord::Base
     belongs_to :film
     
     validates :audio_language, :subtitle_language, :direct_link, :embed_code, presence: true
+    
+    def generated_title
+        return "" if film.nil?
+        
+        title = film.foreign_title;
+        title += " (#{film.english_title})" if film.english_title.present?
+        title += " (Audio: #{audio_language}"
+        if (subtitle_language.present?)
+            title +=  "; Subtitles: #{subtitle_language})"
+        else
+            title += ")";
+        end
+    end
 end
