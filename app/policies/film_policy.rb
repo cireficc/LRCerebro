1e1c56e37221d6b3db094f0d8addd052749ecceb
Christopher Cirefice <christophercirefice@gmail.com>
2016-07-18 22:21:25 +0000

    Films#show viewable when not logged in, but streaming not
    Allow unauthorized access to Films#index and #show, but don't render streaming content unless the user is logged in. Any logged in user has access, as they are MLL faculty, students or staff

--- app/policies/film_policy.rb
+++ app/policies/film_policy.rb
@@ -3,28 +3,27 @@ class FilmPolicy
     attr_reader :user, :film
 
     def initialize(user, film)
-        raise Pundit::NotAuthorizedError unless user
         @user = user
         @film = film
     end
     
     def new?
-        @user.director? || @user.labasst?
+        @user && (@user.director? || @user.labasst?)
     end
     
     def create?
-        @user.director? || @user.labasst?
+        @user && (@user.director? || @user.labasst?)
     end
     
     def edit?
-        @user.director? || @user.labasst?
+        @user && (@user.director? || @user.labasst?)
     end
     
     def update?
-        @user.director? || @user.labasst?
+        @user && (@user.director? || @user.labasst?)
     end
     
     def destroy?
-        @user.director? || @user.labasst?
+        @user && (@user.director? || @user.labasst?)
     end
 end
