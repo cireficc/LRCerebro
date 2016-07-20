63556f3f91ef00f3d649ee905af018961098eacb
Christopher Cirefice <christophercirefice@gmail.com>
2016-07-20 21:04:40 +0000

    CSS + JS for popovers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,8 +42,9 @@ var initialize = function() {
 	    }
 	});
 	
-	// Initialize all Bootstrap toolips
+	// Initialize all Bootstrap toolips and popovers
 	$("[data-toggle='tooltip']").tooltip();
+	$("[data-toggle='popover']").popover({ trigger: "hover focus", container: "body" });
 	
 	// Initialize all acts-as-taggable-on + select2 tag inputs
 	$("*[data-taggable='true']").each(function() {
