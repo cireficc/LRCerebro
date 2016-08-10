0b4db930b308ccf2ac08d0d1db20801522337b95
Christopher Cirefice <christophercirefice@gmail.com>
2016-08-10 19:21:55 +0000

    Rename num_groups to group_size

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808152150) do
+ActiveRecord::Schema.define(version: 20160810191424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,7 +141,7 @@ ActiveRecord::Schema.define(version: 20160808152150) do
     t.string   "name"
     t.text     "description"
     t.integer  "category"
-    t.integer  "num_groups"
+    t.integer  "group_size"
     t.datetime "script_due"
     t.datetime "due"
     t.datetime "viewable_by"
