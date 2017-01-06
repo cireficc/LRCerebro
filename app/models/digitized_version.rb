121cec604c6524208938d078c92ad40064cb8704
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-01-06 18:07:19 -0500

    Update the generated filename using the new fields

--- app/models/digitized_version.rb
+++ app/models/digitized_version.rb
@@ -10,6 +10,19 @@ class DigitizedVersion < ActiveRecord::Base
         filename = film.inventory_item.catalog_number
         filename += " #{film.foreign_title}"
         filename += " (#{film.english_title})" if film.english_title.present?
+
+        filename += ' ['
+
+        if (foreign_title.present? && english_title.present?)
+            filename += "#{foreign_title} - #{english_title}"
+        elsif (foreign_title.present?)
+            filename += foreign_title
+        elsif (english_title.present?)
+            filename += english_title
+        end
+
+        filename += ']'
+
         filename += " (Audio; #{audio_language}"
         if (subtitle_language.present?)
             filename +=  ", Subtitles; #{subtitle_language})"
