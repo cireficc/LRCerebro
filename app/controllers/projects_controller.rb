09fb2aa26de1421443b953f4f9cfc1da8ac89ac6
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-01-06 13:18:40 -0500

    Set archived to false by default to only show current semester data

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,9 @@ class ProjectsController < ApplicationController
         @where[:first_training] = { lte: DateTime.parse(params[:first_training]) } if params[:first_training].present?
         @where[:last_editing] = { lte: DateTime.parse(params[:last_editing]) } if params[:last_editing].present?
         @where[:approved] = params[:approved] if params[:approved].present?
-        @where[:archived] = params[:archived] if params[:archived].present?
+        # Default archived to false if it hasn't been selected out yet
+        params[:archived] = false if params[:archived].blank?
+        @where[:archived] = params[:archived]
         
         # Hack Elasticsearch to gain back the functionality that we had with Pundit scoping
         # Director/labasst have full access to all projects, faculty/students only to their own
