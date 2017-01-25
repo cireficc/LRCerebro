8137c07995aa06b3e963f0a3d15b61d6ef4d3010
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-01-25 12:33:20 -0500

    Decorate User

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,5 +4,17 @@ class UserDecorator < Draper::Decorator
   def self.collection_decorator_class
     PaginatingDecorator
   end
+  
+  def full_name_reverse
+    "#{last_name}, #{first_name}"
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def full_name_with_username
+    "#{last_name}, #{first_name} (#{username})"
+  end
 
 end
