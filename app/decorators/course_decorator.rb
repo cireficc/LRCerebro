4b85bec3e456a2563b42464170d47bbb0a7d0f40
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-01-25 13:34:12 -0500

    Add short_name method back to CourseDecorator

--- app/decorators/course_decorator.rb
+++ app/decorators/course_decorator.rb
@@ -6,6 +6,13 @@ class CourseDecorator < Draper::Decorator
     PaginatingDecorator
   end
 
+  def short_name
+    department_code = Course::DEPARTMENT_CODES.key(Course.departments[department])
+    sektion = "%02d" % section
+
+    "#{department_code} #{course}-#{sektion}"
+  end
+
   def full_name
     "#{name} (#{semester.titleize} #{year})"
   end
