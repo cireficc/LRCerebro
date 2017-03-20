2e79d40bc7e8ca456b48d656b2a358f9a948456d
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-03-20 14:28:40 -0400

    Decorate DigitizedVersion

--- app/models/digitized_version.rb
+++ app/models/digitized_version.rb
@@ -3,32 +3,4 @@ class DigitizedVersion < ActiveRecord::Base
     belongs_to :film
     
     validates :audio_language, presence: true
-    
-    def generated_filename
-        return "" if film.nil?
-
-        filename = film.inventory_item.catalog_number
-        filename += " #{film.foreign_title}"
-        filename += " (#{film.english_title})" if film.english_title.present?
-        filename += " [#{full_title}]" if full_title
-
-        filename += " (Audio; #{audio_language}"
-        if (subtitle_language.present?)
-            filename +=  ", Subtitles; #{subtitle_language})"
-        else
-            filename += ")";
-        end
-    end
-  
-    def full_title
-      if foreign_title.present? && english_title.present?
-        return "#{foreign_title} - #{english_title}"
-      elsif foreign_title.present?
-        return foreign_title
-      elsif english_title.present?
-        return english_title
-      end
-      
-      return nil
-    end
 end
