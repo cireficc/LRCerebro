afe706961739ac47831272c07f4f0351de90cbd5
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-05-08 13:14:34 -0400

    Add role to Enrollment

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,6 +2,14 @@ class Enrollment < ActiveRecord::Base
 
     belongs_to :course
     belongs_to :user, foreign_key: :user_id, primary_key: :pitm
+
+    # User roles in the MLL department and the LRC:
+    # :instructor - Faculty in the MLL department who teach courses
+    # :student - Students in the MLL department who enroll in courses
+    enum role: {
+        instructor: 0,
+        student: 1
+    }
     
     def active?
         self.updated_at > ApplicationConfiguration.last.current_semester_start
