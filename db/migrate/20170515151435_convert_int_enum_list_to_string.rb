291eb6fa9b879f393bc3d65cf22f42547bef7bc7
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-05-15 14:01:37 -0400

    Convert Project.category from int enum to string

---
+++ db/migrate/20170515151435_convert_int_enum_list_to_string.rb
@@ -0,0 +1,23 @@
+class ConvertIntEnumListToString < ActiveRecord::Migration
+  def change
+    change_column :projects, :category, :string
+
+    # Mapping for int enum values to string representation
+    project_categories = {
+        0 => "Camtasia",
+        1 => "Garage Band",
+        2 => "Pixton",
+        3 => "Photo Booth",
+        4 => "Blog",
+        5 => "Pages",
+        6 => "Other",
+        7 => "Storybird"
+    }
+    
+    # Convert the old enum ints to their new string representation
+    Project.all.each do |p|
+      int = p[:category].to_i
+      p.update_attributes(category: project_categories[int])
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509175509) do
+ActiveRecord::Schema.define(version: 20170515151435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(version: 20170509175509) do
     t.integer  "course_id"
     t.string   "name"
     t.text     "description"
-    t.integer  "category"
+    t.string   "category"
     t.integer  "group_size"
     t.datetime "script_due"
     t.datetime "due"
