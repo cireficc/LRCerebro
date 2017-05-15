95bb8f9ff33ebe0e420bf4acb40ec0e356701666
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-05-15 15:44:24 -0400

    Convert Film/DigitizedVersion audio/subtitle languages from int enum to string

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,44 +1,15 @@
 class Language < ActiveRecord::Base
     self.abstract_class = true
-  
-    enum language: {
-        arabic: 0,
-        chinese: 1,
-        english: 2,
-        french: 3,
-        german: 4,
-        italian: 5,
-        japanese: 6,
-        russian: 7,
-        spanish: 8,
-        other: 9
-    }
-    
-    # Pass in a field (value) and the field name to validate the language enum across multiple models
-    # Return the error message to the caller to use with errors.add
-    def self.validate_languages(field)
-        
-        if !field.any?
-            return "need to select a language"
-        end
-        
-        # Remove English because we don't have an English catalog
-        field_temp = field.reject { |l| l == "english" }
-        
-        if !field_temp.any?
-            return "English cannot be the only language selected"
-        end
-        
-        # If any of the audio languages is invalid, add the error and break
-        field.each do |f|
-            valid = false
-            Language.languages.each do |l, i|
-                valid = true if (f == l)
-            end
-            
-            return "not a valid language selection" unless valid
-        end
-        
-        nil
-    end
+
+    LANGUAGES = %w(
+        Arabic
+        Chinese
+        English
+        French
+        German
+        Italian
+        Japanese
+        Russian
+        Spanish
+    )
 end
