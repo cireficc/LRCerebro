cc91898c998b02d9f0b07b4891c79621936ba8c1
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-06-21 17:42:31 -0400

    Add mini project to database

---
+++ db/migrate/20170620214520_create_mini_projects.rb
@@ -0,0 +1,15 @@
+class CreateMiniProjects < ActiveRecord::Migration
+  def change
+    create_table :mini_projects do |t|
+      t.belongs_to :course
+      t.text :resources, array: true, default: []
+      t.text :description
+      t.boolean :supplemental_materials
+      t.datetime :start_date
+      t.datetime :due_date
+      t.datetime :publish_by
+      t.text :publish_methods, array: true, default: []
+      t.timestamps null: false
+    end
+  end
+end
