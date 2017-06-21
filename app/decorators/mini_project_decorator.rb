f21bca6c88b39ed81accf3e00b30fcc0dba029a7
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-06-21 17:43:36 -0400

    Mini project controller, policy, decorator and helper

---
+++ app/decorators/mini_project_decorator.rb
@@ -0,0 +1,19 @@
+class MiniProjectDecorator < Draper::Decorator
+  delegate_all
+
+  def self.collection_decorator_class
+    PaginatingDecorator
+  end
+
+  def stringified_resources
+    resources.join(", ")
+  end
+
+  def stringified_publish_methods
+    publish_methods.join(", ")
+  end
+  
+  def supplemental_materials_string
+    supplemental_materials ? "Yes" : "No"
+  end
+end
