3fc4bdd3565633eee5ca0782dfdb51dd0ec28928
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-07-03 20:05:50 -0400

    Another film decoration method

--- app/decorators/film_decorator.rb
+++ app/decorators/film_decorator.rb
@@ -5,6 +5,18 @@ class FilmDecorator < Draper::Decorator
     PaginatingDecorator
   end
 
+  def full_title_with_catalog_number
+    if foreign_title.present? && english_title.present?
+      return "(#{inventory_item.catalog_number}) #{foreign_title} - #{english_title}"
+    elsif foreign_title.present?
+      return "(#{inventory_item.catalog_number}) #{foreign_title}"
+    elsif english_title.present?
+      return "(#{inventory_item.catalog_number}) #{english_title}"
+    end
+
+    return nil
+  end
+
   def short_description
     h.truncate(description, length: 150)
   end
