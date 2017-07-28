7f37fa8322fe5d28ec5c337751fbae82e8a07efd
Chris Cirefice <cireficc@mail.gvsu.edu>
2017-07-28 09:59:02 -0400

    Decorate film fields (cleanup)

--- app/decorators/film_decorator.rb
+++ app/decorators/film_decorator.rb
@@ -16,6 +16,26 @@ class FilmDecorator < Draper::Decorator
 
     return nil
   end
+  
+  def stringified_audio_languages
+    audio_languages.map { |l| l.titleize }.join(", ")
+  end
+
+  def stringified_subtitle_languages
+    subtitle_languages.map { |l| l.titleize }.join(", ")
+  end
+
+  def stringified_directors
+    directors.map { |d| d.name.titleize }.join(", ")
+  end
+
+  def stringified_cast_members
+    cast_members.map { |c| c.name.titleize }.join(", ")
+  end
+
+  def stringified_genres
+    genres.map { |g| g.name.titleize }.join(", ")
+  end
 
   def short_description
     h.truncate(description, length: 150)
