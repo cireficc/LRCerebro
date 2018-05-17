a24dcb50c711f55bd2c4a193934ccd388daa9bee
Chris Cirefice <cireficc@mail.gvsu.edu>
2018-05-17 14:41:35 -0400

    Migrate catalog_number to Film
    We're about to get rid of `InventoryItem` entirely

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_11_02_102759) do
+ActiveRecord::Schema.define(version: 2018_05_17_182908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2017_11_02_102759) do
     t.datetime "updated_at", null: false
     t.string "cover"
     t.string "more_info_link"
+    t.string "catalog_number"
   end
 
   create_table "games", id: :serial, force: :cascade do |t|
