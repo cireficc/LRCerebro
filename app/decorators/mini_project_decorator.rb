54cc2482a0a004da97fe687600582ddf5d5cc4cc
Chris Cirefice <cireficc@mail.gvsu.edu>
2018-06-08 20:22:26 -0400

    Remove Producteev-related stuff from MiniProject

--- app/decorators/mini_project_decorator.rb
+++ app/decorators/mini_project_decorator.rb
@@ -5,14 +5,6 @@ class MiniProjectDecorator < Draper::Decorator
     PaginatingDecorator
   end
 
-  def producteevified
-    if added_to_producteev?
-      h.content_tag :span, "Yes", class: "label label-success"
-    else
-      h.content_tag :span, "No", class: "label label-danger"
-    end
-  end
-
   def stringified_resources
     resources.reject(&:empty?).join(", ")
   end
