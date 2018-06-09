ac75aed0d24e66d6506d17c4197828d0af5f9a3a
Chris Cirefice <cireficc@mail.gvsu.edu>
2018-06-08 20:37:03 -0400

    Remove Producteev-related stuff from Project

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -5,14 +5,6 @@ class ProjectDecorator < Draper::Decorator
     PaginatingDecorator
   end
   
-  def producteevified
-	  if added_to_producteev?
-		  h.content_tag :span, "Yes", class: "label label-success"
-	  else
-		  h.content_tag :span, "No", class: "label label-danger"
-	  end
-  end
-
   def status
     if approved?
       h.content_tag :span, "Approved", class: "label label-success"
