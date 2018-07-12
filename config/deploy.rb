2e332df51e3389c2eb3a6f65cafbcc073872c8bb
Chris Cirefice <cireficc@mail.gvsu.edu>
2018-07-12 12:21:08 -0400

    Run DelayedJob daemon when application is deployed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "delayed/recipes"
+
 # config valid only for current version of Capistrano
 lock "3.10.2"
 
@@ -44,3 +46,8 @@ namespace :deploy do
     end
   end
 end
+
+set :delayed_job_command, "bin/delayed_job"
+after "deploy:start", "delayed_job:start"
+after "deploy:stop", "delayed_job:stop"
+after "deploy:restart", "delayed_job:stop","delayed_job:start"
