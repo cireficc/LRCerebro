4bbf410301d0ef200abec7bde505ab62638e6dcc
Chris Cirefice <christophercirefice@gmail.com>
2019-08-23 13:41:06 -0400

    Remove broken, useless JS from new_project.js
    Was causing duplicate form submissions. Bug was recently introduced but I don't know where from, duplicate submissions should have been an issue from the start with that code. Could have been from a recent deploy/compiling JS differently in Rails 5, but this bug is squashed

--- app/assets/javascripts/projects/new_project.js
+++ app/assets/javascripts/projects/new_project.js
@@ -5,16 +5,6 @@ $(document).ready(function() {
 	$("#publish_by_hidden").toggle($("#project_present").is(":checked"));
 });
 
-/*
-* Continue the form submission with validation
-*/
-$(document).on("click", "[name='commit']", function(e) {
-	
-	// TODO: validation
-	
-	$(this).closest("form").submit();
-});
-
 // If the checkbox for presenting projects is clicked, toggle the ready by form field
 $(document).on('click', '#project_present', function() {
 	
